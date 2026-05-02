7af295e7fc229346119098a74fab439604826563
iorgwsd <piotrwrt150.com>
2026-05-02 23:47:15 +0200

    update

---
+++ Loader.lua
@@ -0,0 +1,23 @@
+-- Załaduj ten skrypt za pomocą executora!
+-- Pamiętaj aby zmienić link jeśli wrzucisz to na swojego GitHuba
+
+local repoURL = "https://raw.githubusercontent.com/piotrwrt150/zniu81zd41c3jljumqn8p3o609mjr75arzan2r0yxqa/main/"
+
+getgenv().ScoutCheat = {}
+
+print("[ScoutCheat] Ładowanie Config.lua...")
+getgenv().ScoutCheat.Config = loadstring(game:HttpGet(repoURL .. "Config.lua"))()
+
+print("[ScoutCheat] Ładowanie ESP.lua...")
+loadstring(game:HttpGet(repoURL .. "ESP.lua"))()
+
+print("[ScoutCheat] Ładowanie Aimbot.lua...")
+loadstring(game:HttpGet(repoURL .. "Aimbot.lua"))()
+
+print("[ScoutCheat] Ładowanie Visuals.lua...")
+loadstring(game:HttpGet(repoURL .. "Visuals.lua"))()
+
+print("[ScoutCheat] Ładowanie GUI.lua...")
+loadstring(game:HttpGet(repoURL .. "GUI.lua"))()
+
+print("[ScoutCheat] Załadowano pomyślnie! Klawisz K - Menu, L - Zapis configu, J - Wczytanie configu")
